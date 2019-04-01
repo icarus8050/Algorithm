d3be55cedfd51ae1b9c2b4e6cc927d0b93e26b1b
leafy <icarus8050@naver.com>
2019-04-01 12:05:55 +0900

    프로그래머스 (최고의 집합)
    프로그래머스 최고의 집합

---
+++ PGS_MostSet/src/Solution.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Solution {
+
+	public int[] solution(int n, int s) {
+	      int[] answer;
+	      
+	      if (n > s)
+	    	  return new int[] {-1};
+	      
+	      int a = s / n;
+	      int b = s % n;
+	      answer = new int[n];
+	      
+	      Arrays.fill(answer, a);
+	      int idx = answer.length - 1;
+	      while (b > 0) {
+	    	  answer[idx--]++;
+	    	  b--;
+	    	  if (idx == 0) idx = answer.length - 1;
+	      }
+	      
+	      return answer;
+	}
+}
