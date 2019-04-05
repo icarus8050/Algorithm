9f829ad3485b0cfda4496b7054749a953676f895
leafy <icarus8050@naver.com>
2019-04-05 17:37:40 +0900

    Selection sort practice

--- SortAlgorithmPractice/src/SelectionSort.java
+++ SortAlgorithmPractice/src/SelectionSort.java
@@ -1,25 +1,27 @@
 
-
-public class BubbleSort {
+public class SelectionSort {
 
 	public static void main(String[] args) {
 		int[] arr = RandomNumberMaker.getNumber();
 		
 		Util.printArray(arr);
 		System.out.println("------------------------------------------------------");
-		BubbleSort.sort(arr);
+		SelectionSort.sort(arr);
 		Util.printArray(arr);
 	}
 	
 	public static void sort(int[] arr) {
-		for (int i = 0; i < arr.length - 1; i++) {
+		for (int i = 0; i < arr.length; i++) {
+			int minIndex = i;
+			
 			for (int j = i + 1; j < arr.length; j++) {
-				if (arr[i] > arr[j]) {
-					int temp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = temp;
-				}
+				if (arr[minIndex] > arr[j])
+					minIndex = j;
 			}
+			
+			int temp = arr[i];
+			arr[i] = arr[minIndex];
+			arr[minIndex] = temp;
 		}
 	}
 }
