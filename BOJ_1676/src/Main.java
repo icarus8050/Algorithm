c3541bf2a6e6edfe6c9ad322c348ed8773d39433
leafy <icarus8050@naver.com>
2019-04-07 13:23:05 +0900

    BOJ_1676 (팩토리얼 0의 개수)

---
+++ BOJ_1676/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int two = 0;
+		int five = 0;
+		
+		for (int i = 2; i <= N; i *= 2) {
+			two += (N / i);
+		}
+		
+		for (int i = 5; i <= N; i *= 5) {
+			five += (N / i);
+		}
+		System.out.println((two > five) ? five : two);
+		sc.close();
+	}
+}
