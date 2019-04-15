2db6aeebb6489871e621c77fae4f215538e9296e
leafy <icarus8050@naver.com>
2019-04-15 11:08:01 +0900

    BOJ_1509 (팰린드롬 분할)

--- BOJ_1509/src/Main.java
+++ BOJ_1509/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
 	static boolean[][] isPalindrome;
 	static int[] dp;
+	static final int MAX = Integer.MAX_VALUE;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
@@ -17,16 +18,30 @@ public class Main {
 		
 		for (int i = 0; i < len; i++) {
 			isPalindrome[i][i] = true;
-			for (int j = 0; j < i; j++) {
+			for (int j = i - 1; j >= 0; j--) {
 				if (str.charAt(i) == str.charAt(j))
-					isPalindrome[j][i] = isPalindrome[j - 1][i + 1];
+					isPalindrome[j][i] = isPalindrome[j + 1][i - 1];
 				
-				//짝수인 경우
+				//길이가 2인 경우
 				if (i - j == 1)	
 					isPalindrome[j][i] |= (str.charAt(i) == str.charAt(i - 1));
 			}
 		}
 		
+		System.out.println(solve(len - 1));
 		sc.close();
+	}
+	
+	public static int solve(int pos) {
+		if (pos < 0) return 0;
+		if (dp[pos] != -1) return dp[pos];
+		
+		dp[pos] = MAX;
+		for (int i = pos; i >= 0; i--) {
+			if (isPalindrome[i][pos]) {
+				dp[pos] = Math.min(dp[pos], solve(i - 1) + 1);
+			}
+		}
+		return dp[pos];
 	}
 }
