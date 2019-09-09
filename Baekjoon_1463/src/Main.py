2fe2b5d19b9894c77587cfe62d21380b2291da5c
icarus8050 <icarus8050@naver.com>
2019-09-10 00:21:14 +0900

    BOJ_1463 python3 (refactoring)

--- Baekjoon_1463/src/Main.py
+++ Baekjoon_1463/src/Main.py
@@ -1,18 +1,15 @@
-INF = 9999999999
-
 if __name__ == '__main__':
     n = int(input())
-    dp = [INF] * (pow(10, 6) + 1)
+    dp = [0] * (n + 1)
 
     dp[0] = dp[1] = 0
-    dp[2] = dp[3] = 1
 
-    for i in range(4, n + 1):
-        if i % 3 == 0:
-            dp[i] = min(dp[i], dp[i // 3] + 1)
-        if i % 2 == 0:
-            dp[i] = min(dp[i], dp[i // 2] + 1)
-
-        dp[i] = min(dp[i], dp[i - 1] + 1)
+    for i in range(2, n + 1):
+        if i % 3 is 0:
+            dp[i] = min(dp[i // 3], dp[i - 1]) + 1
+        elif i % 2 is 0:
+            dp[i] = min(dp[i // 2], dp[i - 1]) + 1
+        else:
+            dp[i] = dp[i - 1] + 1
 
     print(dp[n])
