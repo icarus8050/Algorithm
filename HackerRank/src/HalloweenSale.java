6ab23f1afdeb6541c8be76b3f039192d5d088257
icarus8050 <icarus8050@naver.com>
2020-01-15 23:58:54 +0900

    Halloween Sale (https://www.hackerrank.com/challenges/halloween-sale/problem) : Fail..

---
+++ HackerRank/src/HalloweenSale.java
@@ -0,0 +1,45 @@
+import java.io.*;
+
+public class HalloweenSale {
+
+    // Complete the howManyGames function below.
+    static int howManyGames(int p, int d, int m, int s) {
+        // Return the number of games you can buy
+        int res = 0;
+
+        while (s > m) {
+            if (p > m) {
+                s -= p;
+                p -= d;
+            } else {
+                s -= m;
+            }
+            res++;
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] pdms = br.readLine().split(" ");
+
+        int p = Integer.parseInt(pdms[0]);
+
+        int d = Integer.parseInt(pdms[1]);
+
+        int m = Integer.parseInt(pdms[2]);
+
+        int s = Integer.parseInt(pdms[3]);
+
+        int answer = howManyGames(p, d, m, s);
+
+        bw.write(String.valueOf(answer));
+        bw.newLine();
+
+        bw.close();
+        br.close();
+    }
+}
