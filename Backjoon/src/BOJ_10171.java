6b6e52ae6854c27af9c854b4f3f4a47e7e8841ec
icarus8050 <icarus8050@naver.com>
2020-02-29 23:30:21 +0900

    BOJ_10171 고양이 (https://www.acmicpc.net/problem/10171)

---
+++ Backjoon/src/BOJ_10171.java
@@ -0,0 +1,11 @@
+/**
+ * 고양이 (https://www.acmicpc.net/problem/10171)
+ */
+public class BOJ_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
