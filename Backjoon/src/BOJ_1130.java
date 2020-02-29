39135e1dcac722ccd1a259043198c46692ec5a79
icarus8050 <icarus8050@naver.com>
2020-02-29 23:26:48 +0900

    BOJ_1330 두 수 비교하기 (https://www.acmicpc.net/problem/1330)

---
+++ Backjoon/src/BOJ_1130.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * 두 수 비교하기 (https://www.acmicpc.net/problem/1330)
+ */
+public class BOJ_1130 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a > b) System.out.println(">");
+        else if (a < b) System.out.println("<");
+        else System.out.println("==");
+
+        sc.close();
+    }
+}
