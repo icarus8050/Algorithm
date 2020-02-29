b04117e08050e2f2aae2c32f04f09383961caa0d
icarus8050 <icarus8050@naver.com>
2020-02-29 23:28:34 +0900

    BOJ_1330 두 수 비교하기 (https://www.acmicpc.net/problem/1330)

--- Backjoon/src/BOJ_1330.java
+++ Backjoon/src/BOJ_1330.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 /**
  * 두 수 비교하기 (https://www.acmicpc.net/problem/1330)
  */
-public class BOJ_1130 {
+public class BOJ_1330 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
