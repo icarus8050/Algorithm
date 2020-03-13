8cb07655c125b7685b9749b282f8c40ab4de16a9
icarus8050 <icarus8050@naver.com>
2020-03-14 00:13:05 +0900

    BOJ_1920 수 찾기 (https://www.acmicpc.net/problem/1920)

---
+++ Backjoon/src/BOJ_1920.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+/**
+ * 수 찾기 (https://www.acmicpc.net/problem/1920)
+ */
+public class BOJ_1920 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        Set<Integer> set = new HashSet<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            if (set.contains(Integer.parseInt(st.nextToken()))) {
+                bw.write(1 + "\n");
+            } else {
+                bw.write(0 + "\n");
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
