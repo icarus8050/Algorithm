e06a8e777a6b24c7c23a5feb45ca02d3a31a70b3
icarus8050 <icarus8050@naver.com>
2020-08-01 23:05:02 +0900

    Bitmap (https://www.acmicpc.net/problem/8061)

--- Backjoon/src/BOJ_8061.java
+++ Backjoon/src/BOJ_8061.java
@@ -2,19 +2,21 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
- * Bitmap (https://www.acmicpc.net/problem/8061) 시간 초과... BFS로 다시 풀어보자
+ * Bitmap (https://www.acmicpc.net/problem/8061)
  */
 public class BOJ_8061 {
 
     static int n, m;
     static int[][] map;
-    static boolean[][] visited;
-    static int minDist;
+    static int[][] visited;
 
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, -1, 0, 1};
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -23,26 +25,26 @@ public class BOJ_8061 {
         n = Integer.parseInt(NM[0]);
         m = Integer.parseInt(NM[1]);
         map = new int[n][m];
+        visited = new int[n][m];
+
+        Queue<Node> queue = new LinkedList<>();
 
         for (int i = 0; i < n; i++) {
             String[] inputs = br.readLine().split("");
             for (int j = 0; j < m; j++) {
                 map[i][j] = Integer.parseInt(inputs[j]);
+
+                if (map[i][j] == 1) {
+                    queue.add(new Node(j, i));
+                    visited[i][j] = 0;
+                }
             }
         }
+        bfs(queue);
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (map[i][j] == 1) {
-                    bw.write(0 + " ");
-                }
-                else {
-                    visited = new boolean[n][m];
-                    visited[i][j] = true;
-                    minDist = Integer.MAX_VALUE;
-                    dfs(j, i, 0);
-                    bw.write(minDist + " ");
-                }
+                bw.write(visited[i][j] + " ");
             }
             bw.newLine();
         }
@@ -52,26 +54,34 @@ public class BOJ_8061 {
         br.close();
     }
 
-    static void dfs(int x, int y, int cnt) {
-        if (cnt >= minDist) return;
-        if (map[y][x] == 1) {
-            minDist = cnt;
-            return;
-        }
+    static void bfs(Queue<Node> queue) {
 
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
 
-            if (isRange(nx, ny) && !visited[ny][nx]) {
-                visited[ny][nx] = true;
-                dfs(nx, ny, cnt + 1);
-                visited[ny][nx] = false;
+            for (int i = 0; i < 4; i++) {
+                int nx = current.x + dx[i];
+                int ny = current.y + dy[i];
+
+                if (isRange(nx, ny) && map[ny][nx] == 0 && visited[ny][nx] == 0) {
+                    visited[ny][nx] = visited[current.y][current.x] + 1;
+                    queue.offer(new Node(nx, ny));
+                }
             }
         }
     }
 
     static boolean isRange(int x, int y) {
         return x >= 0 && x < m && y >= 0 && y < n;
+    }
+
+    static class Node {
+        int x;
+        int y;
+
+        Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
     }
 }
