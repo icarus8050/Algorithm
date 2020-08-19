5b67deb2635abd9ed9a5303039610bf36ac15a7d
icarus8050 <icarus8050@naver.com>
2020-08-19 21:36:41 +0900

    20. Valid Parentheses (https://leetcode.com/problems/valid-parentheses/) Simple solution

--- LeetCode/src/ValidParentheses.java
+++ LeetCode/src/ValidParentheses.java
@@ -10,26 +10,16 @@ public class ValidParentheses {
     }
 
     public static boolean isValid(String s) {
-        char[] ch = s.toCharArray();
-
         Stack<Character> stack = new Stack<>();
-
-        for (char c : ch) {
-            if (c == ')') {
-                if (stack.isEmpty()) return false;
-                Character pop = stack.pop();
-                if (!pop.equals('(')) return false;
-
-            } else if (c == '}') {
-                if (stack.isEmpty()) return false;
-                Character pop = stack.pop();
-                if (!pop.equals('{')) return false;
-            } else if (c == ']') {
-                if (stack.isEmpty()) return false;
-                Character pop = stack.pop();
-                if (!pop.equals('[')) return false;
-            } else {
-                stack.push(c);
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (stack.isEmpty() || stack.pop() != c){
+                return false;
             }
         }
 
