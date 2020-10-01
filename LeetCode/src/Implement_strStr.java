79586e02cfc371969f19007e456ee689d0b701ae
icarus8050 <icarus8050@naver.com>
2020-10-01 19:48:17 +0900

    28. Implement strStr() (https://leetcode.com/problems/implement-strstr/)

---
+++ LeetCode/src/Implement_strStr.java
@@ -0,0 +1,8 @@
+/**
+ * 28. Implement strStr() (https://leetcode.com/problems/implement-strstr/)
+ */
+public class Implement_strStr {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
