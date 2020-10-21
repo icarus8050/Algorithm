f3f4404145bbd935f678a76b5eb4d669a8a6ff89
icarus8050 <icarus8050@naver.com>
2020-10-21 23:45:04 +0900

    100. Same Tree (https://leetcode.com/problems/same-tree/)

---
+++ LeetCode/src/SameTree.java
@@ -0,0 +1,34 @@
+/**
+ * 100. Same Tree (https://leetcode.com/problems/same-tree/)
+ */
+public class SameTree {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q== null) return true;
+        if (p == null || q == null) return false;
+        if (p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+
+        return false;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
