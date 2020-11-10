90f06d2860bdbf7b5387430839d35014f1c66d1e
icarus8050 <icarus8050@naver.com>
2020-11-11 00:00:57 +0900

    23. Merge k Sorted Lists (https://leetcode.com/problems/merge-k-sorted-lists/) (재귀를 이용한 풀이)

--- LeetCode/src/MergeKSortedLists.java
+++ LeetCode/src/MergeKSortedLists.java
@@ -42,6 +42,35 @@ public class MergeKSortedLists {
         return result;
     }
 
+    /* 재귀를 통한 풀이 */
+    public ListNode mergeKLists_2(ListNode[] lists) {
+        return partion(lists, 0, lists.length - 1);
+    }
+
+    private ListNode partion(ListNode[] lists, int s, int e) {
+        if (s == e) return lists[s];
+        if (s < e) {
+            int m = (s + e) / 2;
+            ListNode l1 = partion(lists, s, m);
+            ListNode l2 = partion(lists, m + 1, e);
+            return merge(l1, l2);
+        } else {
+            return null;
+        }
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = merge(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = merge(l1, l2.next);
+            return l2;
+        }
+    }
+
     public static class ListNode {
         int val;
         ListNode next;
