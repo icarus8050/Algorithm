861756bd2b6a3553c11787bfa5a2531652d52813
icarus8050 <icarus8050@naver.com>
2020-11-25 00:07:39 +0900

    88. Merge Sorted Array (https://leetcode.com/problems/merge-sorted-array/) 배열 복사 없이 간결하게 작성한 풀이

--- LeetCode/src/MergeSortedArray.java
+++ LeetCode/src/MergeSortedArray.java
@@ -3,27 +3,16 @@
  */
 public class MergeSortedArray {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] temp = new int[m];
-        if (m >= 0) System.arraycopy(nums1, 0, temp, 0, m);
-
-        int idx1 = 0;
-        int idx2 = 0;
-        int i = 0;
-
-        while (idx1 < m && idx2 < n) {
-            if (temp[idx1] < nums2[idx2]) {
-                nums1[i++] = temp[idx1++];
-            } else {
-                nums1[i++] = nums2[idx2++];
-            }
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            nums1[k--] = (nums1[i] > nums2[j]) ?
+                    nums1[i--] : nums2[j--];
         }
 
-        while (idx1 < m) {
-            nums1[i++] = temp[idx1++];
-        }
-
-        while (idx2 < n) {
-            nums1[i++] = nums2[idx2++];
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
         }
     }
 }
