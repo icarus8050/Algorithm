169c1bdce0cdab59081e55c69de2bd90d0b3a731
icarus8050 <icarus8050@naver.com>
2020-11-27 00:08:19 +0900

    66. Plus One (https://leetcode.com/problems/plus-one/) 다른 풀이 방법

--- LeetCode/src/PlusOne.java
+++ LeetCode/src/PlusOne.java
@@ -23,4 +23,20 @@ public class PlusOne {
 
         return digits;
     }
+
+    public int[] awesomeResolution(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        int[] newDigits = new int[n + 1];
+        newDigits[0] = 1;
+        return newDigits;
+    }
 }
