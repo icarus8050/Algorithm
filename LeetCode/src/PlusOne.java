c991d11732386650faea49738625420d442b233c
icarus8050 <icarus8050@naver.com>
2020-11-27 00:03:31 +0900

    66. Plus One (https://leetcode.com/problems/plus-one/)

---
+++ LeetCode/src/PlusOne.java
@@ -0,0 +1,26 @@
+/**
+ * 66. Plus One (https://leetcode.com/problems/plus-one/)
+ */
+public class PlusOne {
+    public int[] plusOne(int[] digits) {
+        int idx = digits.length - 1;
+        digits[idx]++;
+
+        int carry = 0;
+        while (idx >= 0) {
+            digits[idx] += carry;
+            carry = digits[idx] / 10;
+            digits[idx] %= 10;
+            idx--;
+        }
+
+        if (carry == 1) {
+            int[] ans = new int[digits.length + 1];
+            System.arraycopy(ans, 0, ans, 1, digits.length);
+            ans[0] = 1;
+            return ans;
+        }
+
+        return digits;
+    }
+}
