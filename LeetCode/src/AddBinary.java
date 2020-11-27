aa7c023a5f9f348d8e127f4c39cb40806e618091
icarus8050 <icarus8050@naver.com>
2020-11-28 00:25:52 +0900

    67. Add Binary (https://leetcode.com/problems/add-binary/) (훨씬 짧고 간결한 풀이 방법)

--- LeetCode/src/AddBinary.java
+++ LeetCode/src/AddBinary.java
@@ -37,4 +37,19 @@ public class AddBinary {
 
         return sb.toString();
     }
+
+    public String awesomeResolve(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int i = a.length() - 1, j = b.length() - 1, carry = 0;
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            if (j >= 0) sum += b.charAt(j--) - '0';
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+
+        if (carry != 0) sb.append(carry);
+        return sb.reverse().toString();
+    }
 }
