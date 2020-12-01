74729ad3a253ee7e6f32366ec76937f03cd35821
icarus8050 <icarus8050@naver.com>
2020-12-01 23:51:07 +0900

    83. Remove Duplicates from Sorted List (https://leetcode.com/problems/remove-duplicates-from-sorted-list/)

---
+++ LeetCode/src/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,28 @@
+/**
+ * 83. Remove Duplicates from Sorted List (https://leetcode.com/problems/remove-duplicates-from-sorted-list/)
+ */
+public class RemoveDuplicatesFromSortedList {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode current = head;
+        while (current.next != null) {
+            ListNode next = current.next;
+            if (current.val == next.val) {
+                current.next = next.next;
+            } else {
+                current = next;
+            }
+        }
+
+        return head;
+    }
+
+     public static class ListNode {
+         int val;
+         ListNode next;
+         ListNode() {}
+         ListNode(int val) { this.val = val; }
+         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     }
+}
