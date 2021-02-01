4ed99ceec97b691b5d64420d2a9caff1ce998a03
icarus8050 <icarus8050@naver.com>
2021-02-02 01:02:41 +0900

    괄호 (https://www.acmicpc.net/problem/9012)

--- Backjoon/src/BOJ_9012.java
+++ Backjoon/src/BOJ_9012.java
@@ -4,41 +4,40 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Stack;
 
+/**
+ * 괄호 (https://www.acmicpc.net/problem/9012)
+ */
 public class BOJ_9012 {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int t = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        while (t-- > 0) {
-            String bucket = br.readLine();
-            bw.write(checkVPS(bucket));
-            bw.newLine();
-        }
+        while (n-- > 0) {
+            char[] ps = br.readLine().toCharArray();
 
-        br.close();
-        bw.close();
-    }
-
-    public static String checkVPS(String bucket) {
-        if (bucket.length() % 2 != 0) return "NO";
-
-        Stack<Character> stack = new Stack<>();
-
-        for (int i = 0; i < bucket.length(); i++) {
-            char c = bucket.charAt(i);
+            Stack<Character> stack = new Stack<>();
+            for (char c : ps) {
+                if (stack.isEmpty()) {
+                    stack.push(c);
+                } else if (c == ')' && stack.peek() == '(') {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            }
 
             if (stack.isEmpty()) {
-                stack.push(c);
-            } else if (c == ')' && stack.peek() == '(') {
-                stack.pop();
+                bw.write("YES\n");
             } else {
-                stack.push(c);
+                bw.write("NO\n");
             }
         }
 
-        return stack.isEmpty() ? "YES" : "NO";
+        bw.flush();
+        br.close();
+        bw.close();
     }
 }
