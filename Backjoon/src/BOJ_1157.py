861f83399c729df35efda8b67bacc89e2e2dd63e
icarus8050 <icarus8050@naver.com>
2021-02-10 03:48:13 +0900

    단어 공부 (https://www.acmicpc.net/problem/1157) print를 한 줄로

--- Backjoon/src/BOJ_1157.py
+++ Backjoon/src/BOJ_1157.py
@@ -1,10 +1,5 @@
 # 단어 공부 (https://www.acmicpc.net/problem/1157)
 
 s = input().upper()
-
 wordsCountList = list(map(s.count, [chr(c) for c in range(65, 91)]))
-
-if wordsCountList.count(max(wordsCountList)) > 1:
-    print('?')
-else:
-    print(chr(wordsCountList.index(max(wordsCountList)) + 65))
+print('?' if wordsCountList.count(max(wordsCountList)) > 1 else chr(wordsCountList.index(max(wordsCountList)) + 65))
