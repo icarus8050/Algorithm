b3e8f7187ae546a8ffb1fb1a11b7680aec887d6f
icarus8050 <icarus8050@naver.com>
2021-02-12 20:22:03 +0900

    토마토 (https://www.acmicpc.net/problem/7576) 코드량 감소 풀이

--- Backjoon/src/BOJ_7576.py
+++ Backjoon/src/BOJ_7576.py
@@ -17,9 +17,8 @@ dy = [0, -1, 0, 1]
 m, n = map(int, sys.stdin.readline().split())
 tomatos = [[0 for _ in range(m)] for _ in range(n)]
 visited = [[False for _ in range(m)] for _ in range(n)]
-queue1 = []
-queue2 = []
-days = -1
+queue = []
+days = 0
 
 for i in range(n):
     arr = list(map(int, sys.stdin.readline().split()))
@@ -27,35 +26,26 @@ for i in range(n):
     for j in range(m):
         tomatos[i][j] = arr[j]
         if arr[j] == 1:
-            queue1.append((j, i))
+            queue.append((j, i))
 
 while True:
-    if len(queue1):
-        while len(queue1):
-            position = queue1.pop()
-            for i in range(4):
-                nx = position[0] + dx[i]
-                ny = position[1] + dy[i]
+    nextQueue = []
+    while len(queue):
+        position = queue.pop()
+        for i in range(4):
+            nx = position[0] + dx[i]
+            ny = position[1] + dy[i]
 
-                if isValidPosition(nx, ny) and not visited[ny][nx] and tomatos[ny][nx] == 0:
-                    visited[ny][nx] = True
-                    tomatos[ny][nx] = 1
-                    queue2.append((nx, ny))
-    elif len(queue2):
-        while len(queue2):
-            position = queue2.pop()
-            for i in range(4):
-                nx = position[0] + dx[i]
-                ny = position[1] + dy[i]
+            if isValidPosition(nx, ny) and not visited[ny][nx] and tomatos[ny][nx] == 0:
+                visited[ny][nx] = True
+                tomatos[ny][nx] = 1
+                nextQueue.append((nx, ny))
 
-                if isValidPosition(nx, ny) and not visited[ny][nx] and tomatos[ny][nx] == 0:
-                    visited[ny][nx] = True
-                    tomatos[ny][nx] = 1
-                    queue1.append((nx, ny))
-    days += 1
-
-    if not len(queue1) and not len(queue2):
+    if not nextQueue:
         break
+
+    queue = nextQueue
+    days += 1
 
 if isAllRipen():
     print(days)
