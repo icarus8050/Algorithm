c24be0a3b2ed2bb7840c0a5697bac5a78b651215
icarus8050 <icarus8050@naver.com>
2021-03-11 01:19:42 +0900

    쉬운 계단 수 (https://www.acmicpc.net/problem/10844)

---
+++ Backjoon/src/BOJ_10844.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * 쉬운 계단 수 (https://www.acmicpc.net/problem/10844)
+ */
+public class BOJ_10844 {
+
+    static int[][] dp;
+    static int MOD = 1000000000;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int ans = 0;
+
+        int N = Integer.parseInt(br.readLine());
+        dp = new int[N + 1][11];
+
+        for (int i = 1; i < 10; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i <= N; i++) {
+            dp[i][0] = dp[i - 1][1];
+            for (int j = 1; j < 10; j++) {
+                dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % MOD;
+            }
+        }
+
+        for (int i = 0; i < 10; i++) {
+            ans = (ans + dp[N][i]) % MOD;
+        }
+
+        bw.write(ans + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
